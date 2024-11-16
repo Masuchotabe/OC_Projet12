b00d11b58bdd3826e91980b5a0310ca5389fdc72
Masuchotabe <moreauthomas0@gmail.com>
2024-11-16 19:16:46 +0100

    add sentry configuration for errors

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+import sentry_sdk
 
 load_dotenv()
 
@@ -10,3 +11,10 @@ DATABASE_HOST = os.getenv("DATABASE_HOST")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://3aab9dd5d84c6762712bd92c4d3afc17@o4508309259747328.ingest.de.sentry.io/4508309281964112",
+    traces_sample_rate=1.0,
+)
